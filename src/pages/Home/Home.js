2b1b25daad11aea60a6978c552f9466a42b25fd1
dan1114ho <dan1114ho@gmail.com>
2019-08-14 21:15:39 -0400

    (common) home page example

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Typography } from 'antd';
+import { Page, PageContent, TopNav } from '@containers/Layout';
+import './Home.less';
+
+const { Title } = Typography;
 
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <Page>
+      <TopNav />
+      <PageContent className="home">
+        <Title level={2}>Home</Title>
+      </PageContent>
+    </Page>
+  );
 }
