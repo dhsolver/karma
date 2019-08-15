9687ab67e1a20e72d063d8500f6c16a06a77c62b
dan1114ho <dan1114ho@gmail.com>
2019-08-14 20:14:47 -0400

    (common) TextInput formik field

--- src/components/form/TextInput/TextInput.js
+++ src/components/form/TextInput/TextInput.js
@@ -3,18 +3,28 @@ import { Form, Input } from 'antd';
 import { getIn } from 'formik';
 
 function TextInput(props) {
-  const { field, form, ...rest } = props;
+  const { field, form, ...restProps } = props;
 
   const error = getIn(form.errors, field.name);
   const touched = getIn(form.touched, field.name);
 
+  const hasError = error !== undefined && touched;
+  const isValid = !error && touched;
+
+  const { name, onChange, onBlur, value } = field;
   return (
     <Form.Item
-      hasFeedback={!!(touched && error)}
-      validateStatus={error && 'error'}
-      help={error}
+      validateStatus={hasError ? 'error' : undefined}
+      hasFeedback={isValid}
+      help={(hasError && <li>{error}</li>) || (isValid && '')}
     >
-      <Input {...field} {...rest} />
+      <Input
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        {...restProps}
+      />
     </Form.Item>
   );
 }
