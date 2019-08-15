737f9a1705091f1ff038b347b866ce8d75b59292
dan1114ho <dan1114ho@gmail.com>
2019-08-14 21:09:07 -0400

    (common) update logo to support custom classname

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import cn from 'classnames';
 import { Typography } from 'antd';
 import history from '@utils/history';
 import './Logo.less';
 
 const { Text } = Typography;
 
-export default function Logo() {
+export default function Logo(props) {
+  const { className } = props;
   return (
     <div
-      className="logo"
+      className={cn('logo', className)}
       onClick={() => {
         history.push('/');
       }}
