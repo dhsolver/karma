ca5777126ca5dc5e246de8f12f02a026a68306d9
dan1114ho <dan1114ho@gmail.com>
2019-08-14 20:38:48 -0400

    (common) add private route

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Typography } from 'antd';
+import history from '@utils/history';
 import './Logo.less';
 
 const { Text } = Typography;
 
 export default function Logo() {
   return (
-    <div className="logo">
+    <div
+      className="logo"
+      onClick={() => {
+        history.push('/');
+      }}
+    >
       <Text>BET KARMA</Text>
     </div>
   );
