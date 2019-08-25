db2a8f71e99f049e74985e276fbacdf84a04b799
dan1114ho <dan1114ho@gmail.com>
2019-08-25 13:59:41 -0400

    feat(redux): add articles redux

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as app } from './AppRedux';
 import { reducer as auth } from './AuthRedux';
+import { reducer as articles } from './ArticleRedux';
 
 const reducers = combineReducers({
   app,
-  auth
+  auth,
+  articles
 });
 
 export default reducers;
--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,10 +1,19 @@
-import { all, fork } from 'redux-saga/effects';
-import app from './AppSaga';
-import auth from './AuthSaga';
+import { all, takeLatest } from 'redux-saga/effects';
+import { AppTypes } from './AppRedux';
+import { startup } from './AppSaga';
+import { AuthTypes } from './AuthRedux';
+import { storeToken, clearToken } from './AuthSaga';
+import { ArticleTypes } from './ArticleRedux';
+import { loadArticles } from './ArticleSaga';
 
 /**
  * rootSaga
  */
 export default function* root() {
-  yield all([fork(app), fork(auth)]);
+  yield all([
+    takeLatest(AppTypes.STARTUP, startup),
+    takeLatest(AuthTypes.SET_LOGGED_IN, storeToken),
+    takeLatest(AuthTypes.SET_LOGGED_OUT, clearToken),
+    takeLatest(ArticleTypes.REQUEST_ARTICLES_LIST, loadArticles)
+  ]);
 }
