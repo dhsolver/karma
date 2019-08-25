a3cf61cde9e9d74b8d454880a9d55ea244f57c69
dan1114ho <dan1114ho@gmail.com>
2019-08-25 13:57:07 -0400

    feat(api): add fixtures, add models

---
+++ src/services/fixtures/articles.js
@@ -0,0 +1,82 @@
+const articles = [
+  {
+    title: 'Bales NFL Best Ball Sleepers',
+    article:
+      '<b>Best ball leagues are some of the funnest </b>in the fantasy world because it allows you to draft, but not have to make weekly decisions on who to start and bench. In other words, the most skilled drafters have an edge in these leagues. Drafts can be lost in the early rounds, but they have to be won in the later rounds. That\u2019s what makes sleeper picks so valuable. In this article, I will outline two quarterbacks, three running backs, three wide receivers, and two tight ends with an ADP of 120 or higher (according to FantasyPros). Without further ado, here is my first article in the our new NFL Draft Kit. ',
+    article_image:
+      'https://www.dfskarma.com/wp-content/uploads/2019/06/Derek-Carr.jpg',
+    premium: true,
+    id: '1',
+    author: 'Justin',
+    date: '2019-08-01T15:54:22+00:00',
+    author_image:
+      'http://www.everyonepiano.com/pianomusic/006/0005990/0005990-small.jpg'
+  },
+  {
+    title: 'Bales NFL Best Ball Sleepers',
+    article:
+      '<b>Best ball leagues are some of the funnest </b>in the fantasy world because it allows you to draft, but not have to make weekly decisions on who to start and bench. In other words, the most skilled drafters have an edge in these leagues. Drafts can be lost in the early rounds, but they have to be won in the later rounds. That\u2019s what makes sleeper picks so valuable. In this article, I will outline two quarterbacks, three running backs, three wide receivers, and two tight ends with an ADP of 120 or higher (according to FantasyPros). Without further ado, here is my first article in the our new NFL Draft Kit. ',
+    article_image:
+      'https://www.dfskarma.com/wp-content/uploads/2019/06/Derek-Carr.jpg',
+    premium: false,
+    id: '2',
+    author: 'Justin',
+    date: '2019-08-01T15:54:22+00:00',
+    author_image:
+      'http://www.everyonepiano.com/pianomusic/006/0005990/0005990-small.jpg'
+  },
+  {
+    title: 'Bales NFL Best Ball Sleepers',
+    article:
+      '<b>Best ball leagues are some of the funnest </b>in the fantasy world because it allows you to draft, but not have to make weekly decisions on who to start and bench. In other words, the most skilled drafters have an edge in these leagues. Drafts can be lost in the early rounds, but they have to be won in the later rounds. That\u2019s what makes sleeper picks so valuable. In this article, I will outline two quarterbacks, three running backs, three wide receivers, and two tight ends with an ADP of 120 or higher (according to FantasyPros). Without further ado, here is my first article in the our new NFL Draft Kit. ',
+    article_image:
+      'https://www.dfskarma.com/wp-content/uploads/2019/06/Derek-Carr.jpg',
+    premium: true,
+    id: '3',
+    author: 'Justin',
+    date: '2019-08-01T15:54:22+00:00',
+    author_image:
+      'http://www.everyonepiano.com/pianomusic/006/0005990/0005990-small.jpg'
+  },
+  {
+    title: 'Bales NFL Best Ball Sleepers',
+    article:
+      '<b>Best ball leagues are some of the funnest </b>in the fantasy world because it allows you to draft, but not have to make weekly decisions on who to start and bench. In other words, the most skilled drafters have an edge in these leagues. Drafts can be lost in the early rounds, but they have to be won in the later rounds. That\u2019s what makes sleeper picks so valuable. In this article, I will outline two quarterbacks, three running backs, three wide receivers, and two tight ends with an ADP of 120 or higher (according to FantasyPros). Without further ado, here is my first article in the our new NFL Draft Kit. ',
+    article_image:
+      'https://www.dfskarma.com/wp-content/uploads/2019/06/Derek-Carr.jpg',
+    premium: true,
+    id: '4',
+    author: 'Justin',
+    date: '2019-08-01T15:54:22+00:00',
+    author_image:
+      'http://www.everyonepiano.com/pianomusic/006/0005990/0005990-small.jpg'
+  },
+  {
+    title: 'Bales NFL Best Ball Sleepers',
+    article:
+      '<b>Best ball leagues are some of the funnest </b>in the fantasy world because it allows you to draft, but not have to make weekly decisions on who to start and bench. In other words, the most skilled drafters have an edge in these leagues. Drafts can be lost in the early rounds, but they have to be won in the later rounds. That\u2019s what makes sleeper picks so valuable. In this article, I will outline two quarterbacks, three running backs, three wide receivers, and two tight ends with an ADP of 120 or higher (according to FantasyPros). Without further ado, here is my first article in the our new NFL Draft Kit. ',
+    article_image:
+      'https://www.dfskarma.com/wp-content/uploads/2019/06/Derek-Carr.jpg',
+    premium: true,
+    id: '5',
+    author: 'Justin',
+    date: '2019-08-01T15:54:22+00:00',
+    author_image:
+      'http://www.everyonepiano.com/pianomusic/006/0005990/0005990-small.jpg'
+  },
+  {
+    title: 'Bales NFL Best Ball Sleepers',
+    article:
+      '<b>Best ball leagues are some of the funnest </b>in the fantasy world because it allows you to draft, but not have to make weekly decisions on who to start and bench. In other words, the most skilled drafters have an edge in these leagues. Drafts can be lost in the early rounds, but they have to be won in the later rounds. That\u2019s what makes sleeper picks so valuable. In this article, I will outline two quarterbacks, three running backs, three wide receivers, and two tight ends with an ADP of 120 or higher (according to FantasyPros). Without further ado, here is my first article in the our new NFL Draft Kit. ',
+    article_image:
+      'https://www.dfskarma.com/wp-content/uploads/2019/06/Derek-Carr.jpg',
+    premium: true,
+    id: '6',
+    author: 'Justin',
+    date: '2019-08-01T15:54:22+00:00',
+    author_image:
+      'http://www.everyonepiano.com/pianomusic/006/0005990/0005990-small.jpg'
+  }
+];
+
+export default articles;
