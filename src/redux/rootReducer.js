474411f15b5c5e6423dcdddb4fd6d0f32e52af07
dan1114ho <dan1114ho@gmail.com>
2019-08-26 21:42:08 -0400

    feat(liveodd): add games redux, game model

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as app } from './AppRedux';
 import { reducer as auth } from './AuthRedux';
 import { reducer as articles } from './ArticleRedux';
+import { reducer as games } from './GameRedux';
 
 const reducers = combineReducers({
   app,
   auth,
-  articles
+  articles,
+  games
 });
 
 export default reducers;
